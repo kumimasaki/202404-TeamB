f0be4c4cb5367a22aa005012bf4605e70968ed16
kk22375011 <kk22375011@gmail.com>
2024-06-12 16:07:44 +0900

    CourseController作成

--- TeamB/src/main/java/teamb/com/controllers/UserCourseController.java
+++ TeamB/src/main/java/teamb/com/controllers/UserCourseController.java
@@ -1,14 +1,43 @@
 package teamb.com.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+import teamb.com.models.entity.Admin;
+import teamb.com.models.entity.Lesson;
+import teamb.com.services.LessonService;
 
 @Controller
 public class UserCourseController {
 
-	// ログイン画面の表示
+	@Autowired
+	private HttpSession session;
+	
+	@Autowired
+	private LessonService lessonService;
+	
 	@GetMapping("/user/course/all")
-	public String getAdminLoginPage() {
-		return "user_list.html";
+	public String getUserCourseList(Model model) {
+		//セッションからログインしている人の情報を取得
+		Admin admin = (Admin) session.getAttribute("loginAdminInfo");
+		
+		//もし、admin==null ログイン画面ににリダイレクトする
+		if(admin == null) {
+			return "redirect:/admin/login";
+		} else {
+		//そうでない場合
+		//ログインしている人の名前の情報を画面に渡して講座一覧のhtmlを表示
+			//講座の情報を取得する
+			List<Lesson> lessonList = lessonService.selectAllLessonList(admin.getAdminId());
+			
+			model.addAttribute("adminName", admin.getAdminName());
+			model.addAttribute("lessonList", lessonList);
+			return "user_list.html";
+		}
 	}
 }
