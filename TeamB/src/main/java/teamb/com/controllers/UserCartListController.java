0ff88e0a65c46e23092993d07a4a6368236c29d2
ts22375013 <ts22375013@ga.ttc.ac.jp>
2024-06-17 15:08:19 +0900

    ユーザーのhtml , cssの修正

--- TeamB/src/main/java/teamb/com/controllers/UserCartListController.java
+++ TeamB/src/main/java/teamb/com/controllers/UserCartListController.java
@@ -1,7 +1,9 @@
 package teamb.com.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class UserCartListController {
 	
 	// ログイン画面の表示
